f91d09779bb9dfa0a1a7c060ec2b50265d0a8d94
Yiting Ju <yiting.ju@gmail.com>
2016-05-25 17:28:57 -0700

    A function to find candidate place names
    Create a function which goes through the list of all place names and find a list of candidate place names for a given place name

--- src/edu/ucsb/stko/Candidate.java
+++ src/edu/ucsb/stko/Candidate.java
@@ -46,4 +46,99 @@ public class Candidate {
     public void setPlaceName(String placeName) {
         this.placeName = placeName;
     }
+    
+    
+    public static ArrayList<ArrayList<String>> getFullCandidateListFromFile(String filePath)
+    {
+    	ArrayList<ArrayList<String>> candidateInfoList = new ArrayList<>();
+    	ArrayList<String> placeNames = new ArrayList<>();
+    	//ArrayList<String> placeTypes = new ArrayList<>();
+    	ArrayList<String> dbLinks = new ArrayList<>();
+    	ArrayList<String> lngs = new ArrayList<>();
+    	ArrayList<String> lats = new ArrayList<>();    	
+    	try {
+			CSVReader reader = new CSVReader(new FileReader(filePath), ',');
+			String [] nextLine;
+			while((nextLine = reader.readNext()) != null)
+			{
+				placeNames.add(nextLine[0].toLowerCase());
+				//placeTypes.add(nextLine[1]);
+				dbLinks.add(nextLine[2]);
+				lngs.add(nextLine[3]);
+				lats.add(nextLine[4]);
+			}
+			//System.out.println(placeNames.size());
+			reader.close();			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	
+    	candidateInfoList.add(placeNames);
+    	candidateInfoList.add(dbLinks);
+    	candidateInfoList.add(lngs);
+    	candidateInfoList.add(lats);
+    	return candidateInfoList;
+    }
+    
+    
+    public static ArrayList<ArrayList<String>> getCandidates(String ambiPlaceName, ArrayList<ArrayList<String>> candidateInfoList)
+    {
+    	ArrayList<ArrayList<String>> theCandidates = new ArrayList<>();
+    	ArrayList<String> thePlaceNames = new ArrayList<>();
+    	ArrayList<String> theDbLinks = new ArrayList<>();
+    	ArrayList<String> theLngs = new ArrayList<>();
+    	ArrayList<String> theLats = new ArrayList<>();    	
+    	
+    	ArrayList<String> placeNames = candidateInfoList.get(0);
+    	ArrayList<String> dbLinks = candidateInfoList.get(1);
+    	ArrayList<String> lngs = candidateInfoList.get(2);
+    	ArrayList<String> lats = candidateInfoList.get(3);
+    	for(int i=0; i<placeNames.size(); i++)
+    	{
+    		String placeNameWNoStateName = placeNames.get(i).substring(0, placeNames.get(i).indexOf(","));
+    		if(placeNameWNoStateName.toLowerCase().contains(ambiPlaceName.toLowerCase()))
+    		{
+    			thePlaceNames.add(placeNames.get(i));
+    			theDbLinks.add(dbLinks.get(i));
+    			theLngs.add(lngs.get(i));
+    			theLats.add(lats.get(i));
+    		}
+    	}
+    	theCandidates.add(thePlaceNames);
+    	theCandidates.add(theDbLinks);
+    	theCandidates.add(theLngs);
+    	theCandidates.add(theLats);
+    	
+    	return theCandidates;
+    }
+    
+    
+    public static ArrayList<Candidate> constructCandidateList(String ambiPlaceName, String filePath)
+    {
+    	ArrayList<ArrayList<String>> allCandidates = getFullCandidateListFromFile(filePath);
+    	ArrayList<ArrayList<String>> candidates = getCandidates(ambiPlaceName, allCandidates);
+    	
+    	ArrayList<Candidate> candidateList = new ArrayList<>();
+    	for(int i=0; i<candidates.get(0).size(); i++)
+    	{
+    		String placeEntityName = candidates.get(0).get(i);
+    		double placeEntityLng = Double.parseDouble(candidates.get(2).get(i));
+    		double placeEntityLat = Double.parseDouble(candidates.get(3).get(i));
+    		Candidate entityCandidate= new Candidate(i+1, placeEntityName, placeEntityLng, placeEntityLat);
+    		candidateList.add(entityCandidate);
+    	}
+    	return candidateList;
+    }
+    
+    
+    public static void main(String[] args) {
+    	final String PathToPlaceNameList = "/home/yiting/Dropbox/STKO_ResearchProject/placeNameDisambiguation/TestingDataSet/MostCommonUSNames_wCoords.csv";
+    	ArrayList<Candidate> candidateList = constructCandidateList("Washington", PathToPlaceNameList);
+    	for(Candidate candidate:candidateList)
+    	{
+    		System.out.println(candidate.getId() + ", " + candidate.getPlaceName() + ", " + candidate.getLongitude() + ", " + candidate.getLatitude());
+    	}
+    }
 }
